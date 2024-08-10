38bb955e43dc9a98f495294603d8dd8f94ea2ebd
RITESH <ultimateritesh987@gmail.com>
2024-08-10 20:40:17 +0530

    Rebrand For UltimateOS Signed-off-by: RITESH <ultimateritesh987@gmail.com>

---
+++ src/com/ultimate/experience/colorpicker/SecureSettingColorPickerPreference.java
@@ -0,0 +1,26 @@
+package com.ultimate.experience.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.ultimate.experience.preferences.SecureSettingsStore;
+import com.ultimate.experience.colorpicker.ColorPickerPreference;
+public class SecureSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+
+    public SecureSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SecureSettingsStore(context.getContentResolver()));
+    }
+}
---
+++ src/com/ultimate/experience/colorpicker/SystemSettingColorPickerPreference.java
@@ -0,0 +1,26 @@
+package com.ultimate.experience.colorpicker;
+
+import android.content.Context;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.provider.Settings;
+
+import com.ultimate.experience.preferences.SystemSettingsStore;
+import com.ultimate.experience.colorpicker.ColorPickerPreference;
+public class SystemSettingColorPickerPreference extends ColorPickerPreference {
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorPickerPreference(Context context) {
+        super(context, null);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}
